caa8b7cf056915a4659cd3507f6a539e465e1bbd
Danila Loginov <danila@loginov.rocks>
2025-05-23 20:47:33 -0700

    Web App: Abstract request method in ApiClient and implement refresh tokens

--- web-app/src/ApiClient.mjs
+++ web-app/src/ApiClient.mjs
@@ -3,19 +3,63 @@ export class ApiClient {
     this.baseUrl = baseUrl;
   }
 
-  async getAuth() {
-    const url = `${this.baseUrl}/auth`;
+  async request(endpoint, options = {}) {
+    const { urlSearchParams, withoutAuth, withoutRefresh, ...customParams } = options;
 
-    const response = await fetch(url, {
-      credentials: 'include',
-    });
+    const url = `${this.baseUrl}${endpoint}${urlSearchParams ? `?${urlSearchParams.toString()}` : ''}`;
+
+    const params = {
+      ...customParams,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    if (!withoutAuth) {
+      params.credentials = 'include';
+    }
+
+    let response;
+    try {
+      response = await fetch(url, params);
+    } catch (error) {
+      console.error(`Failed to fetch "${endpoint}":`, error);
+      throw new Error(`Failed to fetch "${endpoint}": ${error.message}`);
+    }
+
+    const contentType = response.headers.get('content-type');
+    const isJson = contentType && contentType.includes('application/json');
 
     if (!response.ok) {
-      throw new Error(`getAuth received response with HTTP status ${response.status}`);
+      // Refresh tokens mechanism: not ideal, but simple.
+      if (response.status === 401 && !withoutRefresh) {
+        // If the request failed with an Unauthorized error and refresh is allowed, try to refresh the tokens first.
+        // If the refresh fails, it will throw a refresh error.
+        await this.postAuthRefresh();
+        // Otherwise, if the refresh succeeded, repeat the original request with the same parameters.
+        return this.request(endpoint, options);
+      }
+
+      const error = new Error(`Unsuccessful response from "${endpoint}"`);
+      error.status = response.status;
+      error.statusText = response.statusText;
+
+      throw error;
+    }
+
+    try {
+      return isJson ? await response.json() : await response.text();
+    } catch (error) {
+      console.error(`Failed to parse successful response from "${endpoint}":`, error);
+      throw new Error(`Failed to parse successful response from "${endpoint}": ${error.message}`);
     }
   }
 
-  async getAuthStrava(redirectUri, state) {
+  getAuth() {
+    return this.request('/auth');
+  }
+
+  getAuthStrava(redirectUri, state) {
     const urlSearchParams = new URLSearchParams({
       redirectUri,
     });
@@ -24,59 +68,29 @@ export class ApiClient {
       urlSearchParams.append('state', state);
     }
 
-    const url = `${this.baseUrl}/auth/strava?${urlSearchParams.toString()}`;
-
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`getAuthStrava received response with HTTP status ${response.status}`);
-    }
-
-    return response.json();
+    return this.request('/auth/strava', {
+      urlSearchParams,
+      withoutAuth: true,
+    });
   }
 
-  async postAuthToken(code, scope, state) {
-    const url = `${this.baseUrl}/auth/token`;
-
-    const response = await fetch(url, {
+  postAuthToken(code, scope, state) {
+    return this.request('/auth/token', {
       body: JSON.stringify({ code, scope, state }),
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
       method: 'post',
+      withoutRefresh: true,
     });
-
-    if (!response.ok) {
-      throw new Error(`postAuthToken received response with HTTP status ${response.status}`);
-    }
   }
 
-  async postAuthLogout() {
-    const url = `${this.baseUrl}/auth/logout`;
-
-    const response = await fetch(url, {
-      credentials: 'include',
-      method: 'post',
-    });
-
-    if (!response.ok) {
-      throw new Error(`postAuthLogout received response with HTTP status ${response.status}`);
-    }
+  postAuthRefresh() {
+    return this.request('/auth/refresh', { method: 'post', withoutRefresh: true });
   }
 
-  async postSyncJob() {
-    const url = `${this.baseUrl}/sync`;
+  postAuthLogout() {
+    return this.request('/auth/logout', { method: 'post' });
+  }
 
-    const response = await fetch(url, {
-      credentials: 'include',
-      method: 'post',
-    });
-
-    if (!response.ok) {
-      throw new Error(`postSyncJob received response with HTTP status ${response.status}`);
-    }
-
-    return response.json();
+  postSyncJob() {
+    return this.request('/sync', { method: 'post' });
   }
 }
